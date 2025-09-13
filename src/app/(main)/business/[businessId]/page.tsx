5cfd33bd5ea6acc8f5b123d1c68f303c8158b50b
leandroveron1110 <leandroveron1110@gmail.com>
2025-09-13 12:33:42 -0300

    params next 15

--- src/app/(main)/business/[businessId]/page.tsx
+++ src/app/(main)/business/[businessId]/page.tsx
@@ -1,15 +1,19 @@
 import BusinessProfile from "@/features/business/components/BusinessProfile";
 import AppHeader from "@/features/header/components/AppHeader";
 
-// Page component
-export default function BusinessPage({ params }: { params: { businessId: string } }) {
+export async function BusinessPage({
+  params,
+}: {
+  params: Promise<{ businessId: string }>;
+}) {
+  const { businessId } = await params;
   return (
     <>
       <AppHeader />
       <div className="min-h-screen bg-gray-100 w-full">
         <main className="w-full">
           <div className="bg-white w-full sm:p-8">
-            <BusinessProfile businessId={params.businessId} />
+            <BusinessProfile businessId={businessId} />
           </div>
         </main>
       </div>
